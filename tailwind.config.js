3da01f80f8b16a892c28048a961708904cc226fb
doobeedoobeedam <kwecitra@gmail.com>
2022-06-25 20:45:37 +0700

    carousel, top rated, sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', "./dist/flowbite/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ]
+}
